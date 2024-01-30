91608d7fc13bfacffbf5497e825ccb5643b7e2d0
naima <naimasabri1504@gmail.com>
2024-01-30 10:25:51 +0100

    commit

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+class Car extends Vehicle {
+    private String color;
+
+    public Car(String make, String model, int rentalPrice, String color) {
+        super(make, model, rentalPrice);
+        this.color = color;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Color: " + color);
+    }
+}
